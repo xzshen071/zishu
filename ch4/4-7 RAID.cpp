5bd7d34eb069ff10279d43c22fd882b72b42cbd0
xzshen071 <43932600+xzshen071@users.noreply.github.com>
2018-11-05 15:59:28 +0800

    Add files via upload

--- ch4/4-7 RAID.cpp
+++ ch4/4-7 RAID.cpp
@@ -1,15 +1,19 @@
 #include<stdio.h>
 #include<string.h>
 #include<math.h>
+#include<iostream>
+using namespace std;
 int d,s,b;
-int num_1[500];//存每列'1'的个数 
-char data[10][500]; 
+int num_1[7000];//存每列'1'的个数 
+char data[10][7000]; 
 char ch_type;
 char table[20]="0123456789ABCDEF"; 
 int is_legal();
 char readchar();
 void Print_data();
 int main(){
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout); 
 	//读入d(行数)，s(比特数)，b(列数)
 	int kase=0;
 	while(scanf("%d",&d)==1 && d!=0){
@@ -28,15 +32,14 @@ int main(){
 		*/
 		//磁盘非法：列方向超过1组xx，列方向校验错误
 		if(is_legal()==0){
+			//cout << "1" << endl;
 			printf("Disk set %d is invalid.\n",++kase);
 		}else{
 			printf("Disk set %d is valid, contents are: ",++kase);
 			Print_data();
 		}
 		
-		
 	} 
-		
 	return 0;
 }
 char readchar(){
@@ -44,7 +47,6 @@ char readchar(){
 		char ch=getchar();
 		if(ch!='\r' && ch!='\n')
 			return ch;
-		
 	}
 }
 //磁盘非法：列方向超过1组xx，列方向校验错误
@@ -64,21 +66,19 @@ int is_legal(){
 			}else if(data[i][j]=='1'){
 				num_1[j]++;
 			} 
-			
 		}
-		
 	}
 	/*
 	for(int j=0;j<s*b;j++){
 		printf("%d",num_1[j]);
 	}
 	*/
-	printf("\n");
+	//printf("\n");
 	//改变x的值，然后判断是否列方向校验错误 
 	for(int j=0;j<s*b;j++){
 		for(int i=0;i<d;i++){
 			//改变x的值 
-			if(data[i][j]=='x'){
+			if(data[i][j]=='x'){		//偶校验 
 				if(ch_type=='E'){
 					if(num_1[j]%2==0)//偶数 
 						data[i][j]='0'; 
@@ -86,15 +86,14 @@ int is_legal(){
 						data[i][j]='1'; 
 						num_1[j]++; 
 					}
-				}else{
+				}else{					//奇校验 
 					if(num_1[j]%2==0){//偶数 
 						data[i][j]='1'; 
 						num_1[j]++; 
 					}else
 						data[i][j]='0'; 
 				} 
-			}
-				
+			}	
 		}
 		if(ch_type=='E' && num_1[j]%2!=0){//偶：每一列一'1'的个数为偶数 
 			//printf("2\n");
@@ -110,7 +109,6 @@ int is_legal(){
 		printf("%s\n",data[i]);
 	}
 	*/
-	
 	return 1;
 }
 //打印数据 
@@ -118,14 +116,15 @@ void Print_data(){
 	int Judge_x=0,Judge_y=0; 
 	//注意校验位
 	int flag=4,sum=0;
-	for(int j=0;j<b;j++){
-		for(int i=0;i<d;i++){
-			for(int k=0;k<s;k++){
-				//忽视校验位 
+	for(int j=0;j<b;j++){			//数据块数 
+		for(int i=0;i<d;i++){		//行数 
+			for(int k=0;k<s;k++){	//每个数据块的列数 
+				//忽视校验位，每d个数据块就有一个校验位 
 				if(i==Judge_x && j==Judge_y){
 					//printf("%d %d\n",i,j);
-					Judge_x++;
-					Judge_y=(Judge_y+1)%d;
+					if(Judge_x == (d-1)) Judge_x = 0;
+					else Judge_x++;
+					Judge_y++;
 					break;
 				}
 				//有没有4位 
@@ -139,16 +138,12 @@ void Print_data(){
 					flag=4;
 					sum=0;
 				}
-				
-				
 			}
 		}
 	}
-	
 	if(flag==4 ){
 		printf("\n");
-	}else
+	}else{
 		printf("%c\n",table[sum]);
-	
-	
+	}
 }
