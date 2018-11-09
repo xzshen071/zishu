6b8ee657d570c40a52f0c78a5c84bf23c5a2057c
xzshen071 <43932600+xzshen071@users.noreply.github.com>
2018-11-09 16:42:37 +0800

    Add files via upload

--- ch3/3-4 Uva455.cpp
+++ ch3/3-4 Uva455.cpp
@@ -3,35 +3,35 @@
 #include<string.h>
 
 int main(){
-	//周期串
-	//枚举法，它的周期最大是它本身
-	char s[1000];
-	scanf("%s",s);
-	int len=strlen(s);
-	for(int i=1;i<=len;i++){//枚举 
-		int flag=1; 
-		if(len%i==0){//如果i为最小周期，那么字符串长度必定是i的整数倍
-			for(int j=i;j<len;j++){//判断i是否为其周期 
-				if(s[j-i]!=s[j]){
-					flag=0;
-					break;
-				}
-			}
-			if(flag){
-				printf("%d\n",i); 
-				break;//只打印最小周期 
-			}
-			
-			
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	
+	int t;
+	while(scanf("%d", &t) == 1) {
+		int symbol = 1;
+		while(t--) {
+			if(symbol) symbol = 0;
+			else printf("\n");
+			//枚举法，它的周期最大是它本身
+			char s[100];
+			scanf("%s", s);
+			int len = strlen(s);
+			for(int i = 1; i <= len; i++) {//枚举 
+				int flag = 1; 
+				if(len % i == 0) {//如果i为最小周期，那么字符串长度必定是i的整数倍
+					for(int j = i; j < len; j++) {//判断i是否为其周期 
+						if(s[j-i] != s[j]) {
+							flag = 0;
+							break;
+						}
+					}
+					if(flag) {
+						printf("%d\n", i); 
+						break;//只打印最小周期 
+					}
+				}	
+			}	
 		}
-		
-		
-	}	
-	
-	
-	
-	
-	
-	
+	}
 	return 0;
 }
