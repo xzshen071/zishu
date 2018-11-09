1a6e7bfb49219d64e19c7fbb1a93adccc9a4f605
xzshen071 <43932600+xzshen071@users.noreply.github.com>
2018-11-09 16:25:36 +0800

    Add files via upload

--- ch3/3-3 Uva1225.cpp
+++ ch3/3-3 Uva1225.cpp
@@ -1,23 +1,58 @@
-#include<stdio.h>
-#include<math.h>
-#include<string.h>
-int main(){
-	//数数字
-	char s[10006];
-	scanf("%s",s);
-	int len=strlen(s),num[10];
-	memset(num,0,sizeof(num));
-	for(int i=0;i<len;i++){
-		for(int j=0;j<10;j++)
-			if(s[i]-'0'==j)
-				num[j]++;
-	}	
-	for(int i=0;i<10;i++){
-         printf("%d:%d\n",i,num[i]);
-	} 
+#include<iostream>
+#include<sstream>
+#include<cstring>
+#include<string>
+using namespace std;
+
+const int maxn = 10 + 5;	
+
+int num[maxn];				//存0~9依次出现的次数 
+string to_s(int a);
+int main() {
+	//string tmp = to_s(985432);
+	//cout << tmp << " " << tmp.size() << endl;
 	
-	
-	
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	int n;
+	while(cin >> n) {
+		while(n--) {
+			memset(num, 0 ,sizeof(num));
+			int t;
+			cin >> t;
+			//对从1~n的整数按string类型处理 
+			for(int i = 1; i <= t; i++) {
+				string tmp = to_s(i);
+				//cout << tmp << " " << tmp.size() << endl;
+				for(int j = 0; j < tmp.size(); j++) {
+					if(tmp[j] == '0') num[0]++;
+					else if(tmp[j] == '1') num[1]++;
+					else if(tmp[j] == '2') num[2]++;
+					else if(tmp[j] == '3') num[3]++;
+					else if(tmp[j] == '4') num[4]++;
+					else if(tmp[j] == '5') num[5]++;
+					else if(tmp[j] == '6') num[6]++;
+					else if(tmp[j] == '7') num[7]++;
+					else if(tmp[j] == '8') num[8]++;
+					else if(tmp[j] == '9') num[9]++;
+				} 
+			}
+			int flag = 1;
+			for(int i = 0; i < 10; i++) {
+				if(flag) flag = 0;
+				else cout << " ";
+				cout << num[i];
+			}
+			cout << endl; 
+		}	
+	}
 	
 	return 0;
+}
+string to_s(int a) {
+	stringstream ss;
+	ss << a;
+	string s = ss.str();
+	
+	return s;
 }
