cb2ce4f7e9df5c926b7244fa93a6bf68bfa57f9c
xzshen071 <43932600+xzshen071@users.noreply.github.com>
2018-11-09 17:47:43 +0800

    Add files via upload

--- ch3/3-5 Uva227.cpp
+++ ch3/3-5 Uva227.cpp
@@ -1,25 +1,32 @@
 #include<stdio.h>
 #include<math.h>
 #include<string.h>
+#include<iostream>
+#include<ctype.h>
 #define maxn 5
+using namespace std;
 
 int main(){
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
 	//谜题  
 	int t = 0;
 	char a[maxn][maxn];//字符型也可以是二维的
 	//读入 
     while(1){
         //读入
-        if(t){
-    		printf("\n");
-		}
-        fgets(a[0],10,stdin);//可以这么读，最多读10个，但是以\n结尾（若每行均以\n结束） 
+        if(t) getchar();
+        fgets(a[0],10,stdin);//可以这么读，最多读10个，但是以\n结尾（若每行均以\n结束）
+		//cout << a[0] << endl;
+		 
         int ok=1;
         if(a[0][0] == 'Z') break;//判断的是第一个字母
+        if(t) cout << endl;
         int m = 0, n = 0;
         for(int i = 0; i <= 4; ++i) {
-            if (i) {//不是第一行
+            if(i) {//不是第一行
                 fgets(a[i],10,stdin);
+                //cout << a[i] << endl;
             }
             for(int j = 0; j <= 4; ++j) {
                 if(a[i][j]  == ' ' || a[i][j] == 0) {//注意 前四个位置是否有空，或第五个位置（字符串尾）为空不显示！且若||前面的成立不读后面的！
@@ -28,22 +35,26 @@ int main(){
                 }
             }
         }
+        //cout << m << " " << n << endl;
         //移动 
         char c;
-    	while((c=getchar())!='\n'){
-    		if(c!='0'){
+    	while(c = getchar()) {
+    		//cout << c << endl;
+    		if((isalpha(c) || isdigit(c)) && c != '0'){
     			switch(c){
     				case 'A':
     					if(m-1<0){
     						ok=0;
+    						//cout << c << " " << "1" << endl;
 						}else{
 							a[m][n]=a[m-1][n];
 							m--;
 						}
 						break;
 					case 'B':
-						if(m+1>maxn){
+						if(m+1>=maxn){
     						ok=0;
+    						//cout << c << " " << "2" << endl;
 						}else{
 							a[m][n]=a[m+1][n];	
 							m++;
@@ -51,8 +62,9 @@ int main(){
 
 						break;
 					case 'R':
-    					if(n+1>maxn){
+    					if(n+1>=maxn){
     						ok=0;
+    						//cout << c << " " << "3" << endl;
 						}else{
 							a[m][n]=a[m][n+1]; 
 							n++;
@@ -61,21 +73,26 @@ int main(){
     				case 'L':
     					if(n-1<0){
     						ok=0;
+    						//cout << c << " " << "4" << endl;
 						}else{
 							a[m][n]=a[m][n-1];
 							n--;	
 						}
     					break;
     				default:
+    					//cout << c << " " << "5" << endl;
 						ok=0;
 						break; 
 				}
     			a[m][n]=' ';
     			
-			}//else
-				//brek;//不要这两句，会导致getchar()没接收到'\n'而导致下一次输入发生错误，即接受了还没接收的'\n' 
+			}else if(c == '0') {
+				//cout << "0" << endl;
+				break; 
+			}
     			
 		}
+		
 		//输出 
 		printf("Puzzle #%d:\n",++t);
 		if(ok){
@@ -88,15 +105,9 @@ int main(){
 				printf("\n");
 			}
 		}else
-			printf("This puzzle has no final configuration\n");
-	
+			printf("This puzzle has no final configuration.\n");
+		
 	}   	
-	
-	
-	
-	
-	
-	
 	
 	return 0;
 }
