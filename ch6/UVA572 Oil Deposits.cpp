ab4717e5c09afec85aa23fffcc6019eb0234b848
xzshen071 <sxz0701pro@163.com>
2019-09-08 17:13:28 +0800

    Modify UVA572

--- ch6/UVA572 Oil Deposits.cpp
+++ ch6/UVA572 Oil Deposits.cpp
@@ -1,32 +1,51 @@
 #include<cstdio>
-#include<cstring>
 #include<iostream>
+#include<cstring>
 using namespace std;
 
+const int maxm = 100 + 5;
 const int maxn = 100 + 5;
-char s[maxn][maxn];
-int idl[maxn][maxn], cnt, m, n;
+char oil[maxm][maxn];
+int v[maxm][maxn];
+int m, n;
 
-//将连通的点标记连通标记
-void dfs(int x, int y, int id) {
-	if(x<0 || x>=m || y<0 || y>=n) return;
-	if(idl[x][y] > 0 || s[x][y] == '*') return;
-	idl[x][y] = id;
-	for(int dx = -1; dx <= 1; dx++) 
+void DFS(int x, int y) {
+	if(x < 0 || x >= m || y < 0 || y >= n) return;	//越界 
+	if(v[x][y] == 1 || oil[x][y] == '*') return;
+	v[x][y] = 1;
+	for(int dx = -1; dx <= 1; dx++)
 		for(int dy = -1; dy <= 1; dy++)
-			if(dx!=0 || dy!=0) dfs(x+dx, y+dy, id); 
-} 
+			if(dx != 0 || dy != 0) DFS(x+dx, y+dy);  
+	//cout << x << " " << y << endl;
+	
+	return;
+}
 int main() {
 	//freopen("input.txt", "r", stdin);
-	//freopen("output.txt", "w", stdout);
-	while(scanf("%d%d", &m, &n) == 2 && m && n) {
-		for(int i = 0; i < m; i++) scanf("%s", s[i]);
-		memset(idl, 0, sizeof(idl));
-		cnt = 0;
-		for(int i = 0; i < m; i++)
-			for(int j = 0; j < n; j++) 
-				if(idl[i][j] == 0 && s[i][j] == '@') dfs(i, j, ++cnt);
-		cout << cnt << endl;	
+	while(cin >> m >> n) {
+		int count = 0;								//存结果 
+		memset(v, 0, sizeof(v));
+		if(m == 0) break;
+		//输入 
+		for(int i = 0; i < m; i++) {
+			scanf("%s", oil[i]);
+		}  
+		/*
+		for(int i = 0; i < m; i++) 
+			cout << oil[i] << endl;
+		*/
+		//处理 
+		for(int i = 0; i < m; i++) {
+			for(int j = 0; j < n; j++) {
+				if(v[i][j] == 1 || oil[i][j] == '*') continue;
+				count++;
+				//cout << "start:" << i << " " << j << endl; 
+				DFS(i, j);
+			}
+		}
+		cout << count << endl;	
 	}
+
 	return 0;
-} 
+}
+ 
